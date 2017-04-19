6bf0342d854e64a9d3e0bc5cecf9705790cb4b21
Paulo Chaves <opaulochaves@gmail.com>
2017-04-18 22:47:37 -0300

    polish ui

--- src/index.js
+++ src/index.js
@@ -5,11 +5,10 @@ import jsImg from './assets/js.png';
 const page = `
 <div class="main">
   <h1>ES6 + Webpack 2 starter</h1>
-  <h2>Yes, another boilerplate!</h2>
   <div>
     <img src="${jsImg}" alt="JavaScript"/>
   </div>
-  <h2>I'm Paulo Chaves from Piauí, Brazil</h2>
+  <h2>Paulo Chaves from Piaui, Brazil</h2>
 </div>
 `;
 
